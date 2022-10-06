4a924ffbb9b3d8a464c661f1a5b7d286f04c96af
Blender Defender <defenderblender@gmail.com>
2022-10-06 16:26:22 +0200

    refactor: Register Properties differently (function `register_props`); Use Blender naming conventions; Add support for SAM; Add location property to bl_info, Add leading 0 to the version number to follow the Semantic versioning scheme.

--- __init__.py
+++ __init__.py
@@ -37,63 +37,68 @@ from bpy.types import (
 
 bl_info = {
     "name": "Super Render Farm",
-    "author": "Crafto1337, Pidgeon Tools",
-    "version": (0, 1),
+    "author": "Crafto1337, Pidgeon Tools, Blender Defender",
+    "version": (0, 0, 1),
     "blender": (2, 93, 0),
+    # TODO: Maybe find a shorter description. This description is very long.
     "description": "Setup the render farm directy from Blender without having to start and setting it up manually",
+    "location": "Properties > Render > Super Render Farm",
     "warning": "",
     "tracker_url": "https://www.github.com/PidgeonTools/SuperRenderFarm/issues",
     "wiki_url": "https://www.github.com/PidgeonTools/SuperRenderFarm",
+    "endpoint_url": "https://raw.githubusercontent.com/PidgeonTools/SAM-Endpoints/main/SuperRenderFarm.json",
     "category": "Render",
 }
 
-class properties(PropertyGroup):
-    string: StringProperty(
-        name = "String",
-        description = "Test",
-        default = ""
-    )
+# class properties(PropertyGroup):
+#     string: StringProperty(
+#         name = "String",
+#         description = "Test",
+#         default = ""
+#     )
     
-    bool: BoolProperty(
-        name = "Bool",
-        description = "Test",
-        default = False
-    )
+#     bool: BoolProperty(
+#         name = "Bool",
+#         description = "Test",
+#         default = False
+#     )
 
-    int: IntProperty(
-        name = "Int",
-        description = "Test",
-        default = 0,
-        min = 0,
-        max = 10
-    )
+#     int: IntProperty(
+#         name = "Int",
+#         description = "Test",
+#         default = 0,
+#         min = 0,
+#         max = 10
+#     )
 
-    float: FloatProperty(
-        name = "Float",
-        description = "Test",
-        default = 0.0,
-        min = 0.0,
-        max = 10.0
-    )
+#     float: FloatProperty(
+#         name = "Float",
+#         description = "Test",
+#         default = 0.0,
+#         min = 0.0,
+#         max = 10.0
+#     )
     
-    enum: EnumProperty(
-        name = "Enum",
-        description = "",
-        items = [
-                ('OP1', "Test", "")
-        ],
-        #update=LoadPreset
-    )
-    
+#     enum: EnumProperty(
+#         name = "Enum",
+#         description = "",
+#         items = [
+#                 ('OP1', "Test", "")
+#         ],
+#         #update=LoadPreset
+#     )
+
+def register_properties():
+    s = bpy.types.Scene
     #-----  -----#
     
-    test_render_time: BoolProperty(
+    s.test_render_time = BoolProperty(
         name = "Test Render Time",
         description = "Render one test frame to approximate the render time per frame",
         default = False
     )
 
-    render_time: IntProperty(
+    s.render_time = IntProperty(
         name = "Render Time",
         description = "",
         default = 0,
@@ -101,7 +106,7 @@ class properties(PropertyGroup):
         #max = 10
     )
 
-    file_format: EnumProperty(
+    s.file_format = EnumProperty(
         name = "File Format",
         description = "Because you have a video format selected, you now have the option to change that.",
         items = [
@@ -128,13 +133,13 @@ class properties(PropertyGroup):
         #update=LoadPreset
     )
     
-    video: BoolProperty(
+    s.video = BoolProperty(
         name = "Generate Video",
         description = "",
         default = False
     )
     
-    fps: IntProperty(
+    s.fps = IntProperty(
         name = "Video FPS",
         description = "",
         default = 24,
@@ -142,7 +147,7 @@ class properties(PropertyGroup):
         #max = 10
     )
     
-    vrc: EnumProperty(
+    s.vrc = EnumProperty(
         name = "Video Rate Control",
         description = "",
         items = [
@@ -152,7 +157,7 @@ class properties(PropertyGroup):
         #update=LoadPreset
     )
     
-    vrc_value: IntProperty(
+    s.vrc_value = IntProperty(
         name = "Video Rate Control Value",
         description = "Test",
         default = 0,
@@ -160,13 +165,13 @@ class properties(PropertyGroup):
         #max = 10
     )
     
-    resize: BoolProperty(
+    s.resize = BoolProperty(
         name = "Resize the video",
         description = "",
         default = False
     )
     
-    res_x: IntProperty(
+    s.res_x = IntProperty(
         name = "New video width",
         description = "Test",
         default = 1920,
@@ -174,7 +179,7 @@ class properties(PropertyGroup):
         #max = 10
     )
     
-    res_y: IntProperty(
+    s.res_y = IntProperty(
         name = "New video height",
         description = "Test",
         default = 1080,
@@ -182,7 +187,7 @@ class properties(PropertyGroup):
         #max = 10
     )
 
-    exit_blender: BoolProperty(
+    s.exit_blender = BoolProperty(
         name = "Exit Blender",
         description = "",
         default = False
@@ -203,7 +208,6 @@ class SRF_OT_render_button(Operator):
 
 def render_srf(context):
     scene = context.scene
-    props = scene.properties
     
     import json
     import sys
@@ -214,44 +218,44 @@ def render_srf(context):
 
     jO = {
     "VER": bpy.app.version_string,
-    "FS": bpy.context.scene.frame_start,
-    "FE": bpy.context.scene.frame_end,
-    "RE": bpy.context.scene.render.engine,
-    "FF": bpy.context.scene.render.image_settings.file_format,
-    "RT": props.render_time
+    "FS": scene.frame_start,
+    "FE": scene.frame_end,
+    "RE": scene.render.engine,
+    "FF": scene.render.image_settings.file_format,
+    "RT": scene.render_time
     }
 
-    if props.render_time_test:
+    if scene.test_render_time:
         startTime = time.time()
         bpy.ops.render.render()
         jO["RT"] = time.time() - startTime
 
     if jO["FF"] in ["AVI_JPEG", "AVI_RAW", "FFMPEG"]:
-        jO["FF"] = props.file_format
+        jO["FF"] = scene.file_format
 
-    jO["V"] = props.video
+    jO["V"] = scene.video
 
     if jO["V"]:
-        jO["FPS"] = bpy.context.scene.render.fps#props.fps
-        jO["VRC"] = props.vrc
-        jO["VRCV"] = props.vrc_value
+        jO["FPS"] = scene.render.fps #scene.fps
+        jO["VRC"] = scene.vrc
+        jO["VRCV"] = scene.vrc_value
 
-        jO["R"] = props.resize
+        jO["R"] = scene.resize
 
         if jO["R"]:
-            jO["RESX"] = props.res_x
-            jO["RESY"] = props.res_y
+            jO["RESX"] = scene.res_x
+            jO["RESY"] = scene.res_y
 
     jS = json.dumps(jO)
 
     subprocess.call([sys.executable, context.preferences.addons["Super Render Farm"].preferences.script_location, "--", jS], creationflags=CREATE_NEW_CONSOLE)
 
-    if props.exit_blender:
+    if scene.exit_blender:
         bpy.ops.wm.quit_blender()
  
 class SRF_PT_panel(Panel):
     bl_label = "Super Render Farm"
-    bl_idname = "OBJECT_SRF_PANEL"
+    bl_idname = "SRF_PT_panel"
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
     bl_context = "render"
@@ -259,43 +263,42 @@ class SRF_PT_panel(Panel):
     def draw(self, context):
         layout = self.layout
         scene = context.scene
-        props = scene.properties
 
         row = layout.row()
-        row.prop(props, "test_render_time")
+        row.prop(scene, "test_render_time")
 
-        if not props.test_render_time:
+        if not scene.test_render_time:
             row = layout.row()
-            row.prop(props, "render_time")
+            row.prop(scene, "render_time")
 
         if bpy.context.scene.render.image_settings.file_format in ["AVI_JPEG", "AVI_RAW", "FFMPEG"]:
             row = layout.row()
-            row.prop(props, "file_format")
+            row.prop(scene, "file_format")
         
         row = layout.row()
-        row.prop(props, "video")
+        row.prop(scene, "video")
         
-        if props.video:
+        if scene.video:
             #row = layout.row()
-            #row.prop(props, "fps")
+            #row.prop(scene, "fps")
             
             row = layout.row()
-            row.prop(props, "vrc")
+            row.prop(scene, "vrc")
             
             row = layout.row()
-            row.prop(props, "vrc_value")
+            row.prop(scene, "vrc_value")
             
             row = layout.row()
-            row.prop(props, "resize")
+            row.prop(scene, "resize")
             
-            if props.resize:
+            if scene.resize:
                 row = layout.row()
-                row.prop(props, "res_x")
+                row.prop(scene, "res_x")
                 #row = layout.row()
-                row.prop(props, "res_y")
+                row.prop(scene, "res_y")
 
             row = layout.row()
-            row.prop(props, "exit_blender")
+            row.prop(scene, "exit_blender")
 
         row = layout.row()
         row = layout.row()
@@ -313,24 +316,27 @@ class SRF_APT_Preferences(AddonPreferences):
         layout.prop(self, "script_location")
 
 classes = (
-    properties,
+    # properties,
     SRF_OT_render_button,
     SRF_PT_panel,
     SRF_APT_Preferences
 )
 
 def register():
+    register_properties()
+
     for cls in classes:
         bpy.utils.register_class(cls)
+
     
-    bpy.types.Scene.properties = bpy.props.PointerProperty(type = properties)
+    # bpy.types.Scene.properties = bpy.props.PointerProperty(type = properties)
 
 
 def unregister():
     for cls in reversed(classes):
         bpy.utils.register_class(cls)
     
-    del bpy.types.Scene.properties
+    # del bpy.types.Scene.properties
 
 
 if __name__ == "__main__":
